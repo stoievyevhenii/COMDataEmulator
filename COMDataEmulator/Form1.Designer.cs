4b74d369ba46dab3347bd621711c17aa56b82d9f
Yevhenii Stoiev <stoiev.yevhenii@outlook.com>
2022-02-20 14:59:26 +0200

    Add project files.

---
+++ COMDataEmulator/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace COMDataEmulator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.modeComboBox = new System.Windows.Forms.ComboBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.DataForSend = new System.Windows.Forms.RichTextBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.COMPortsComboBox = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.statusBar = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusField = new System.Windows.Forms.ToolStripStatusLabel();
+            this.COMSendTimeout = new System.Windows.Forms.TextBox();
+            this.writeTimeout = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.modeComboBox);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(632, 51);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Режим";
+            // 
+            // modeComboBox
+            // 
+            this.modeComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.modeComboBox.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.modeComboBox.FormattingEnabled = true;
+            this.modeComboBox.Location = new System.Drawing.Point(7, 20);
+            this.modeComboBox.Name = "modeComboBox";
+            this.modeComboBox.Size = new System.Drawing.Size(619, 21);
+            this.modeComboBox.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.groupBox2.Controls.Add(this.DataForSend);
+            this.groupBox2.Location = new System.Drawing.Point(13, 181);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(632, 350);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Данные для отправки";
+            // 
+            // DataForSend
+            // 
+            this.DataForSend.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.DataForSend.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.DataForSend.Location = new System.Drawing.Point(3, 16);
+            this.DataForSend.Margin = new System.Windows.Forms.Padding(10);
+            this.DataForSend.Name = "DataForSend";
+            this.DataForSend.Size = new System.Drawing.Size(626, 331);
+            this.DataForSend.TabIndex = 0;
+            this.DataForSend.Text = "";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox3.Controls.Add(this.writeTimeout);
+            this.groupBox3.Controls.Add(this.COMSendTimeout);
+            this.groupBox3.Controls.Add(this.COMPortsComboBox);
+            this.groupBox3.Location = new System.Drawing.Point(13, 71);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(632, 104);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "COM Порт для отправки";
+            // 
+            // COMPortsComboBox
+            // 
+            this.COMPortsComboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.COMPortsComboBox.FormattingEnabled = true;
+            this.COMPortsComboBox.Location = new System.Drawing.Point(7, 19);
+            this.COMPortsComboBox.Name = "COMPortsComboBox";
+            this.COMPortsComboBox.Size = new System.Drawing.Size(619, 21);
+            this.COMPortsComboBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(570, 541);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Запуск";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(489, 541);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusBar,
+            this.statusField});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 572);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(657, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // statusBar
+            // 
+            this.statusBar.Name = "statusBar";
+            this.statusBar.Size = new System.Drawing.Size(0, 17);
+            // 
+            // statusField
+            // 
+            this.statusField.Name = "statusField";
+            this.statusField.Size = new System.Drawing.Size(118, 17);
+            this.statusField.Text = "toolStripStatusLabel1";
+            // 
+            // COMSendTimeout
+            // 
+            this.COMSendTimeout.Location = new System.Drawing.Point(7, 68);
+            this.COMSendTimeout.Name = "COMSendTimeout";
+            this.COMSendTimeout.Size = new System.Drawing.Size(106, 20);
+            this.COMSendTimeout.TabIndex = 2;
+            // 
+            // writeTimeout
+            // 
+            this.writeTimeout.AutoSize = true;
+            this.writeTimeout.Location = new System.Drawing.Point(7, 49);
+            this.writeTimeout.Name = "writeTimeout";
+            this.writeTimeout.Size = new System.Drawing.Size(106, 13);
+            this.writeTimeout.TabIndex = 3;
+            this.writeTimeout.Text = "Интервал отправки";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(657, 594);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox modeComboBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RichTextBox DataForSend;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ComboBox COMPortsComboBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel statusBar;
+        private System.Windows.Forms.ToolStripStatusLabel statusField;
+        private System.Windows.Forms.TextBox COMSendTimeout;
+        private System.Windows.Forms.Label writeTimeout;
+    }
+}
+
---
+++ COMDataEmulator/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO.Ports;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace COMDataEmulator
+{
+    public partial class Form1 : Form
+    {
+        static bool activeSend = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitComPorts();
+            InitSendDataMode();
+        }
+
+        private void InitSendDataMode()
+        {
+            try
+            {
+                var modes = new[] { "ASCII", "byte", "HEX" };
+                foreach (var mode in modes)
+                {
+                    modeComboBox.Items.Add(mode);
+                }
+            }
+            catch (Exception e)
+            {
+                statusField.Text = e.Message;
+            }
+
+            statusField.Text = "";
+        }
+        private void InitComPorts()
+        {
+            try
+            {
+                var portsArray = SerialPort.GetPortNames();
+
+                foreach (var port in portsArray)
+                {
+                    COMPortsComboBox.Items.Add(port);
+                }
+            }
+            catch (Exception e)
+            {
+                statusField.Text = e.Message;
+            }
+        }
+
+        // Form elements actions
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var enteredText = DataForSend.Text;
+            var comPort = COMPortsComboBox.SelectedItem.ToString();
+            var sendTimeout = int.Parse(COMSendTimeout.Text);
+
+            Thread thred = new Thread(() => StartDataSend(comPort, sendTimeout, enteredText));
+            thred.Start();
+
+            button1.Enabled = false;
+            button2.Enabled = true;
+        }
+        private void StartDataSend(string comPort, int sendTimeout, string data)
+        {
+            try
+            {
+                SerialPort _serialPort = new SerialPort(comPort, 9600, Parity.None, 8, StopBits.One);
+                _serialPort.Handshake = Handshake.None;
+                _serialPort.WriteTimeout = sendTimeout;
+
+                while (activeSend)
+                {
+                    try
+                    {
+                        if (!(_serialPort.IsOpen))
+                        {
+                            _serialPort.Open();
+                            _serialPort.Write(data);
+                        };
+                    }
+                    catch (Exception e)
+                    {
+                        statusField.Text = e.Message;
+                    }
+                }
+
+                _serialPort.Close();
+            }
+            catch (Exception e)
+            {
+                statusField.Text = e.Message;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StopSend();
+        }
+
+        private void StopSend()
+        {
+            activeSend = false;
+
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
